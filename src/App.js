9e0fb93a27c2a38567392b3d3eaf6acf2ebc778f
PaulaE89 <paeb08@gmail.com>
2020-09-23 11:54:44 -0500

    title and image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import welcomeImage from './assets/welcome.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
 
+
+    return (
+
+      <div className="row d-flex justify-content-center">
+
+        <div className="col-md-12">
+          <p className="welcome-title">So exicted to have you on board</p>
+        
+          <img src={welcomeImage} />
+        </div>
+      </div>
+
+    );
+
+  }
+}
 export default App;
